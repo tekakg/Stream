1250e1d523b67487017fd458f9e98a0449581ab3
Aditya <akgupta@tekion.com>
2023-02-21 11:10:33 +0530

    create object

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,18 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
 
-        List<Integer> list1=new ArrayList<>();
-        list1.add(1);
-        list1.add(2);
-        list1.add(3);
-        Stream<Integer> stream=list1.stream();
-        List<Integer>evenList=stream.filter(i->i%2==0).collect(Collectors.toList());
-        System.out.println(evenList );
+        //creating empty object.
+        Stream stream=Stream.empty();
+
+        //array, object and collection.
+        String[] arr={"abc","def","ghi"};
+        List<Integer>list=new ArrayList<>();
+        list.add(1);list.add(2);list.add(3);
+        Stream stream1=Stream.of(list);
+        stream1.forEach(i->{System.out.println(i);});
+
+        // using builder function.
+        Stream stream2=Stream.builder().build();
+
     }
 }
